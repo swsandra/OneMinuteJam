b505d30fdcbe4e8eef0dfec1ff11f850f36f844c
Carlos Rivero <carlosrivero96@gmail.com>
2022-10-03 13:52:27 -0400

    Removed action delay

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    bool canInteract = true;
     [SerializeField] Transform actionPoint;
-    [SerializeField] float actionCooldown = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,55 +15,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canInteract) {
-            bool action = false;
-            if (Input.GetAxisRaw("Vertical") > 0f) {
-                // Change sprite to back
-                GetComponent<Animator>().SetBool("facingDown", false);
-                Debug.Log(Input.GetAxisRaw("Vertical"));
+        if (Input.GetAxisRaw("Vertical") > 0f) {
+            // Change sprite to back
+            GetComponent<Animator>().SetBool("facingDown", false);
+            Debug.Log(Input.GetAxisRaw("Vertical"));
 
-                // move actionPoint up
-            }
-            else if (Input.GetAxisRaw("Vertical") < 0f) {
-                // Change sprite to front
-                GetComponent<Animator>().SetBool("facingDown", true);
-                Debug.Log(Input.GetAxisRaw("Vertical"));
-                
-                // move actionPoint down
-            }
-            if (Input.GetKeyDown(KeyCode.J)) {
-                Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
-                if (col && col.GetComponent<Pumpkin>().interactable) {
-                    col.GetComponent<Pumpkin>().Carve();
-                    col.GetComponent<Pumpkin>().interactable = false;
-                    action = true;
+            // move actionPoint up
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f) {
+            // Change sprite to front
+            GetComponent<Animator>().SetBool("facingDown", true);
+            Debug.Log(Input.GetAxisRaw("Vertical"));
+            
+            // move actionPoint down
+        }
+        if (Input.GetKeyDown(KeyCode.J)) {
+            Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
+            if (col && col.GetComponent<Pumpkin>().interactable) {
+                col.GetComponent<Pumpkin>().Carve();
+                col.GetComponent<Pumpkin>().interactable = false;
                 }
-            }
-            else if (Input.GetKeyDown(KeyCode.K)) {
-                Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
-                if (col && col.GetComponent<Pumpkin>().interactable) {
-                    col.GetComponent<Pumpkin>().Lit();
-                    col.GetComponent<Pumpkin>().interactable = false;
-                    action = true;
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.L)) {
-                Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
-                if (col && col.GetComponent<Pumpkin>().interactable) {
-                    col.GetComponent<Pumpkin>().Explode();
-                    col.GetComponent<Pumpkin>().interactable = false;
-                    action = true;
-                }
-            }
-            if (action) {
-                canInteract = false;
-                StartCoroutine(interactionCooldown());
+        }
+        else if (Input.GetKeyDown(KeyCode.K)) {
+            Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
+            if (col && col.GetComponent<Pumpkin>().interactable) {
+                col.GetComponent<Pumpkin>().Lit();
+                col.GetComponent<Pumpkin>().interactable = false;
             }
         }
-    }
-
-    IEnumerator interactionCooldown() {
-        yield return new WaitForSeconds(actionCooldown);
-        canInteract = true;
+        else if (Input.GetKeyDown(KeyCode.L)) {
+            Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
+            if (col && col.GetComponent<Pumpkin>().interactable) {
+                col.GetComponent<Pumpkin>().Explode();
+                col.GetComponent<Pumpkin>().interactable = false;
+            }
+        }
     }
 }
