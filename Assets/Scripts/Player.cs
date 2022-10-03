8cb073b7d9b87ee08418b9d69c16daac6b066548
Carlos Rivero <carlosrivero96@gmail.com>
2022-10-03 14:01:07 -0400

    Restricted vertical movement and actionPoint now moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Transform actionPoint;
+    [Header("HardMode")]
+    [SerializeField] bool hardMode;
+    [SerializeField] float actionPointDown = -0.2f;
+    [SerializeField] float actionPointUp = 0.3f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,19 +19,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Vertical") > 0f) {
+        if (hardMode && Input.GetAxisRaw("Vertical") > 0f) {
             // Change sprite to back
             GetComponent<Animator>().SetBool("facingDown", false);
             Debug.Log(Input.GetAxisRaw("Vertical"));
 
             // move actionPoint up
+            actionPoint.localPosition = new Vector2(0, actionPointUp);
         }
-        else if (Input.GetAxisRaw("Vertical") < 0f) {
+        else if (hardMode && Input.GetAxisRaw("Vertical") < 0f) {
             // Change sprite to front
             GetComponent<Animator>().SetBool("facingDown", true);
             Debug.Log(Input.GetAxisRaw("Vertical"));
             
             // move actionPoint down
+            actionPoint.localPosition = new Vector2(0, actionPointDown);
+
         }
         if (Input.GetKeyDown(KeyCode.J)) {
             Collider2D col = Physics2D.OverlapPoint(actionPoint.position);  // Check Collision
